2e61cce14fccbb1a13829d9f29334d48285e1e03
najibkado <dopeland3@gmail.com>
2022-09-07 19:23:52 +0100

    Initial Commit

---
+++ main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "main/index.html")
+
+def form(request):
+    return render(request, "main/form.html")
