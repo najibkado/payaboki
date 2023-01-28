40e7c647acd9a34f1fd5ba37e5716518fbe8bcd0
Najib Kado <najibkado@gmail.com>
2023-01-28 14:59:39 +0100

    Admin set

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Transaction, Escrow
+from .models import Transaction, Escrow, Wallet
 
 # Register your models here.
 admin.site.register(Transaction)
 admin.site.register(Escrow)
+admin.site.register(Wallet)
